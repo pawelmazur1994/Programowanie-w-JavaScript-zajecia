010622e573232f5207ee78567a2c806de1d4ae74
pawelmazur1994 <66783018+pawelmazur1994@users.noreply.github.com>
2022-10-06 09:40:11 +0200

    Pierwsze laby

---
+++ lab1/srcipt.js
@@ -0,0 +1,27 @@
+// const liczba1 = document.querySelector('#liczba1');
+// const liczba2 = document.querySelector('#liczba2');
+// const liczba3 = document.querySelector('#liczba3');
+// const newElement = document.createElement('input.liczby')
+// const btn = document.querySelector('#przelicz');
+
+// const przelicz = () => {
+//     console.log(`Wynik dodawania: ${+liczba1.value + +liczba2.value}`);
+//     console.log(`Wynik odejmowania: ${+liczba1.value - +liczba2.value}`);
+//     console.log(`Wynik dzielenia: ${+liczba1.value / +liczba2.value}`);
+//     console.log(`Wynik mnożenia: ${+liczba1.value * +liczba2.value}`);
+// }
+
+// // Wersja shame
+
+//  btn.addEventListener('click', przelicz);
+
+
+// // Wersja zieew
+
+//  liczba1.addEventListener('keyup', przelicz);
+//  liczba2.addEventListener('keyup', przelicz);
+
+// Wersja normal
+
+
+
